e79ee8c804723d70e34e8fa02a2113d71d7844ae
Longvathanak14 <111101964+Longvathanak14@users.noreply.github.com>
2023-03-04 09:02:50 +0700

    sidebar

---
+++ Responsive_Sidebar_With_Collapsing/script.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+    const sidebar = document.querySelector(".sidebar");
+    const closeBtn = document.querySelector("#btn");
+    const searchBtn = document.querySelector(".bx-search")
+
+    closeBtn.addEventListener("click",function(){
+        sidebar.classList.toggle("open")
+        menuBtnChange()
+    })
+
+    searchBtn.addEventListener("click",function(){
+        sidebar.classList.toggle("open")
+        menuBtnChange()
+    })
+
+
+   
+}
+let changeIcon = function(icon){
+    icon.classList.toggle(' fa-solid fa-circle-right ')
+}
